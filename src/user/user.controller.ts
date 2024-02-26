a98214e37b1e060d2bd496ebdb9e7a4c592fedab
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-26 12:13:52 -0300

    passando as rotas para o service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
+import service from "./service";
 
 class userController{
-   router: Router()
+    router: Router;
+
+   constructor(){
+    this.router = Router()
+    this.initRoutes();
+   }
+
+   initRoutes(){
+    this.router.get("/", service.getTeste)
+   }
 }
 
 export default new userController();

cd1249ffe15704649dffcee35d204b8ba67d3bc5
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-27 09:56:08 -0300

    feat: criando metodo create, para criação de usuarios

--- src/user/service.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-class userService {
-  public getTeste(req: Request, res: Response) {
-    return res.json({ ok: true });
-  }
-}
-
-export default new userService();

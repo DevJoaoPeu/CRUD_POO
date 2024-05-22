d89708b2587e00f0423d7cd5ea760d400d57f173
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-05-21 21:21:50 -0300

    feat: commit bomba haha

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,17 @@ import userController from "./user/user.controller";
 const PORT = 3333
 const app = Express();
 
+// rota get --- http://localhost:3333/26bc72a4-2d22-4006-b3db-0fdc358c6d8e
+// rota post ---- http://localhost:3333/create
+// rota put ---- http://localhost:3333/update/26bc72a4-2d22-4006-b3db-0fdc358c6d8e
+// rota delete ---- http://localhost:3333/delete/26bc72a4-2d22-4006-b3db-0fdc358c6d8e
+
 app.use(Express.json())
-app.use(userController.router);
+
+app.post('/create', userController.create);
+app.get("/:userId", userController.getUserById)
+app.put("/update/:userId", userController.update);
+app.delete("/delete/:userId", userController.deleteUser);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,91 @@
-import { Router } from "express";
-import userService from "./user.service";
+import { Request, Response, Router } from "express";
+import { prisma } from "../prisma/api";
 
-class userController{
-    router = Router()
+interface PropsBody {
+  name: string;
+  email: string;
+  password: string;
+}
 
-   constructor(){
-    this.initRoutes();
-   }
+interface PropsBodyPut {
+  name: string;
+  email: string;
+  password: string;
+  id: string;
+}
 
-   initRoutes(){
-    this.router.get("/", userService.getTeste)
-    this.router.post("/create", userService.create)
-   }
+class userController {
+  router: Router = Router();
+
+  async deleteUser(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+      const response = await prisma.user.delete({ where: { id: userId } });
+      return res.json({ message: "Usuário deletado com sucesso!", response });
+    } catch (error) {
+      return res.json({ message: error.message });
+    }
+  }
+
+  public async getUserById(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+      const response = await prisma.user.findFirst({ where: { id: userId } });
+      if (!response) {
+        return res.json({
+          message: "Usuario não encontrado",
+        });
+      }
+
+      return res.json(response);
+    } catch (error) {
+      return res.json({ message: error.message });
+    }
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    try {
+      const { name, email, password }: PropsBody = req.body;
+
+      const response = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+
+      return res.json({
+        message: "Usuário criado com sucesso!",
+        response,
+      });
+    } catch (error) {
+      return res.json({
+        message: error.message,
+      });
+    }
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    try {
+      const { userId } = req.params;
+      const { name, email, password }: PropsBodyPut = req.body;
+
+      const user = await prisma.user.update({
+        where: { id: userId },
+        data: { name, email, password },
+      });
+
+      return res.json({
+        message: "Usuário atualizado com sucesso!",
+        user,
+      });
+    } catch (error) {
+      return res.json({
+        message: error.message,
+      });
+    }
+  }
 }
 
 export default new userController();
